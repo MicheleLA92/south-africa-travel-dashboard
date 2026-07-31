1fd1bd6b69486aeb9eb14d68b538ad8fa256a62d
Michele <michele@example.local>
2026-07-31 11:55:44 +0000

    Auto collapse sidebar on mobile

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_page_config(
     page_title="Südafrika Reise",
     page_icon="🌍",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="auto",
 )
 
 CSS = """
